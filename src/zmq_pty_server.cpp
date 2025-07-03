6bad70526e74928c2a70c566cbdc38516e1875bb
KS4Nguyen <sebastian.nguyen86@gmail.com>
2025-07-03 17:41:08 +0200

    Last Sync: 2025-07-03 17:41 (Mobile)

--- src/zmq_pty_server.cpp
+++ src/zmq_pty_server.cpp
@@ -1,74 +1,185 @@
-#include <cstdio>
 #include <iostream>
 #include <string>
-#include <chrono>
-
-//#include <unistd.h>
+#include <assert.h>
+//#include <algorithm> // Required for std::sort
+#include "unistd.h"
 
 #include <zmqpp/zmqpp.hpp>
-#include <assert.h>
+//#include "options.hpp"
 
-#ifdef _POSIX_
-  #include "pthreads.h"
-#else
-  #include <thread>
+#ifndef BUILD_CLIENT_NAME
+  #define BUILD_CLIENT_NAME "zmqpp_pty_client"
 #endif
 
-//#include "common.h"
+//#include "pthreads.h"
+#include <thread>
+#include <mutex>
 
-#define VERSION "0.1"
+#include <atomic>
+
 
 using namespace std;
 
+#define VERSION "0.0.2"
+
+/******************************************************************************
+ * @name    printhelp()
+ * @brief   Usage of the program.
+ ******************************************************************************/
+
+void printhelp()
+{
+   cout << "Usage: zmq_client <Endpoint> <Socket-Type> [Options]\n";
+   cout << "  Endpoint:    Format like tcp://127.0.0.1:4242";
+   cout << "  Socket-Type: One od the following:";
+   cout << "               pub, sub, push, pull, req, res";
+   cout << "  Options:";
+   cout << "  -v  Verbose mode.";
+   cout << "  -h  Peint this help.";
+}
+
+/******************************************************************************
+ * @name    receive()
+ * @brief   Receive ZMQ message, thread.
+ * @param   *s      Pointer to ZMQ socket, where to grab incomming messages.
+ * @param   *buff   Pointer to messae buffer.
+ ******************************************************************************/
+
+///@{
+atomic<int> new_msg (0);
+mutex mtx_get_message; // mutex for all resourcrs shared with recept()
+
+void get_message( zmqpp::socket *s, vector<string> *buff )
+{
+  zmqpp::message rx_msg;
+  string *string_msg;
+  string_msg = (string*)&rx_msg; // TODO Check if this is a safe type conversion.
+
+  while ( NULL != s ) {
+    if ( true == s->receive(rx_msg, false) ) { // TODO lock socket aka *s.
+      mtx_get_message.lock();
+      //lock_guard<std::mutex> lock( mtx_get_message );
+        buff->push_back( *string_msg );
+        new_msg++;
+      mtx_get_message.unlock();
+      //lock_guard<std::mutex> unlock( mtx_get_message);
+    }
+  }
+}
+ ///@}
+
+/******************************************************************************
+ * @name    main()
+ * @brief   Connect to ZMQ socket, send message and listen to server answer.
+ ******************************************************************************/
+
 const string version = VERSION;
 
-int main(int argc, char *argv[]) {
-  const string endpoint = "tcp://*:4242";
-  int major, minor, patch;
-  string zmqver;
-  
-  zmq_version (&major, &minor, &patch);
 
-/*
-#ifdef _COMMON_HELPERS
-  zmqver = concat( itoc( sizeof( long int ), (long int *)major ), ".", \
-                   itoc( sizeof( long int ), (long int *)minor ), ".", \
-                   itoc( sizeof( long int ), (long int *)patch ) \
-                 );
-#else
-  #ifdef _STRING_H
-  zmqver = to_char(mayor) + "." + to_char(minor) + "." + to_char(patch);
-  #else
-  zmqver = 0.0.0;
-  #endif
-#endif
-*/
-  
-  printf ("0MQ version: %d.%d.%d\n", major, minor, patch);
+int main( int argc, char *args[] )
+{
+  int  rc = 0;           // return code assertion
+  int verbose = 0;
 
-  // initialize the 0MQ context
+  const string pname    = args[0];
+  const string endpoint = args[1];
+  const string stype    = args[2];
+  
   zmqpp::context context;
+  zmqpp::socket_type type;
+  bool socket_initialized = false;
+  
+  vector<string> rx_buff;
 
-  // generate a pull socket
-  zmqpp::socket_type type = zmqpp::socket_type::reply;
-  zmqpp::socket socket (context, type);
+  zmqpp::message tx_msg;  // send message
+  string *tx_msg_string;
+  tx_msg_string = (string*)&tx_msg;
 
-  // bind to the socket
-  socket.bind(endpoint);
-  string text;
+  if ( stype == "pub"  ) { type = zmqpp::socket_type::pub; }  else
+  if ( stype == "sub"  ) { type = zmqpp::socket_type::sub; }  else
+  if ( stype == "push" ) { type = zmqpp::socket_type::push; } else
+  if ( stype == "pull" ) { type = zmqpp::socket_type::pull; } else
+  if ( stype == "req"  ) { type = zmqpp::socket_type::req; }  else
+  //if ( stype == "res"  ) { type = zmqpp::socket_type::res; } else
+  {
+      cout << "Error: Unknown socket type!" << endl;
+      printhelp();
+      return ( -2 );
+  }
+  
+  zmqpp::socket socket( context, type );
+  socket.bind( endpoint );
+  socket.connect( endpoint );
+  socket_initialized = true;
 
-  while (1) {
-    // receive the message
-    zmqpp::message message;
-    // decompose the message 
-    socket.receive(message);
+  /****************************************************
+   * @description    Compose a message from a string
+   ****************************************************/
 
-    message >> text;
+  ///@{
 
-    //Do some 'work'
-    this_thread::sleep_for(chrono::seconds(1));
-    cout << "Received Hello" << endl;
-    socket.send("World");
+  //int number
+  //rx_buff >> text >> number;
+
+  ///@}
+
+  /****************************************************
+   * @description    Listen to socket till abort
+   ****************************************************/
+
+  ///@{
+
+  std::thread thr_receive(
+      get_message,      // void(*)(socket*, vector*)
+      &socket,          // zmqpp::socket*
+      &rx_buff          // std::vector<std::string>*
+  );
+
+  //thr_receive.detach();
+  
+  while ( 1 ) {
+    if ( new_msg > 0 ) {
+      if ( mtx_get_message.try_lock() ) {
+        cout << rx_buff[new_msg-1] << endl;
+
+        // Discard latest message
+        rx_buff.pop_back();
+        new_msg--;
+
+        // TODO Make buffer a FIFO:
+        //std::sort( rx_buff.begin(), rx_buff.end() );
+
+        mtx_get_message.unlock();
+      }
+    }
+    std::getline( cin, *tx_msg_string);
+    socket.send( tx_msg );
+  }
+  ///@}
+
+  if ( socket_initialized )
+    socket.close();
+
+  thr_receive.join();
+  if ( verbose == 1 ) {
+    cout << "Terminating (" << rc << ")" << endl;
   }
 
+  return rc;
 }
+
+/**
+ * @TODO  Replace send() & receive() with raw data handling
+ *
+
+  bool zmqpp::socket::receive_raw  (   char *    buffer,
+    size_t &    length,
+    int const   flags = normal
+  )
+
+  bool zmqpp::socket::send_raw  (   char const *    buffer,
+    size_t const    length,
+    int const   flags = normal
+  )
+
+*/
